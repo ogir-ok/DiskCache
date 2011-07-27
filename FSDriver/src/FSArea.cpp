c9c56536149633b7e7542165dbe3c0a7279e0d79
anakriya <anakriya@mail.ru>
2011-07-27 19:13:25 +0300

    added something

---
+++ FSDriver/src/FSArea.cpp
@@ -0,0 +1,39 @@
+/*
+ * FSArea_imp.cpp
+ *
+ *  Created on: Jul 27, 2011
+ *      Author: user
+ */
+#include "FSArea.h"
+#include <sys/types.h>
+#include <unistd.h>
+
+FSArea_imp::FSArea_imp(const char* fileName)
+{
+	this->_streamId=open(fileName,O_RDWR|O_SYNC);
+	pthread_mutex_init(&(this->_lock),NULL);
+}
+
+FSArea_imp::~FSArea_imp()
+{
+	pthread_mutex_destroy(&(this->_lock));
+	close(this->_streamId);
+}
+
+void* FSArea_imp::GetBlock(int blockId)
+{
+	pthread_mutex_lock(&(this->_lock));
+	lseek(this->_streamId,blockId*BLOCK_SIZE,SEEK_SET);
+	void *buff;
+	read(this->_streamId,buff,BLOCK_SIZE);
+	pthread_mutex_unlock(&(this->_lock));
+	return buff;
+}
+
+void FSArea_imp::SetBlock(int blockId,void* value)
+{
+	pthread_mutex_lock(&(this->_lock));
+	lseek(this->_streamId,blockId*BLOCK_SIZE,SEEK_SET);
+	write(this->_streamId,value,BLOCK_SIZE);
+	pthread_mutex_unlock(&(this->_lock));
+}
