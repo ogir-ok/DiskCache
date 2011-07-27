40da24ef37b18c841897293aee4f4d10af86f482
Garagatyy Igor <ogirok.07@mail.ru>
2011-07-27 19:47:22 +0300

    Deleted Debug/

--- FSDriver/Debug/src/FSArea.d
+++
@@ -1,3 +0,0 @@
-src/FSArea.d src/FSArea.o: ../src/FSArea.cpp ../src/FSArea.h
-
-../src/FSArea.h:
--- FSDriver/Debug/src/FSDriver.d
+++
@@ -1,6 +0,0 @@
-src/FSDriver.d src/FSDriver.o: ../src/FSDriver.cpp ../src/FSDriver.h \
- ../src/FSArea.h
-
-../src/FSDriver.h:
-
-../src/FSArea.h:
