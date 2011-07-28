cdd73c0a04f7dc693bd8e79281dd8aa9e0f0c785
Garagatyy Igor <ogirok.07@mail.ru>
2011-07-28 14:57:07 +0300

    Headers for DiskCache

---
+++ DiskCache/src/BuffFreeList.h
@@ -0,0 +1,14 @@
+/*
+ * BuffFreeList.h
+ *
+ *  Created on: Jul 28, 2011
+ *      Author: user
+ */
+
+#ifndef BUFFFREELIST_H_
+#define BUFFFREELIST_H_
+
+
+
+
+#endif /* BUFFFREELIST_H_ */
---
+++ DiskCache/src/DiskCache.h
@@ -0,0 +1,14 @@
+/*
+ * DiskCache.h
+ *
+ *  Created on: Jul 28, 2011
+ *      Author: user
+ */
+
+#ifndef DISKCACHE_H_
+#define DISKCACHE_H_
+
+
+
+
+#endif /* DISKCACHE_H_ */
