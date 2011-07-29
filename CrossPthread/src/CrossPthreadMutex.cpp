c25d6fc89eb7415fc20d72d61b45165233377928
anakriya <anakriya@mail.ru>
2011-07-29 14:02:36 +0300

    realization cpp PthreadMutex

--- CrossPthread/src/CrossPthreadMutex.cpp
+++ CrossPthread/src/CrossPthreadMutex.cpp
@@ -8,18 +8,20 @@
 
 CrossPthreadMutex_imp_unix::CrossPthreadMutex_imp_unix()
 {
-
+	pthread_mutex_init(&(this->_mutex),NULL);
 }
 CrossPthreadMutex_imp_unix::~CrossPthreadMutex_imp_unix()
 {
+	pthread_mutex_destroy(&(this->_mutex));
 
 }
 void CrossPthreadMutex_imp_unix::Lock()
 {
+	pthread_mutex_lock(&(this->_mutex));
 
 }
 void CrossPthreadMutex_imp_unix::Unlock()
 {
-
+	pthread_mutex_unlock(&(this->_mutex));
 }
 
