05b5222e9e719a8594ce7138c013786da4a3cf10
Garagatyy Igor <ogirok.07@mail.ru>
2011-07-29 18:31:02 +0300

    Crossfile

--- CrossFile/src/CrossFile.cpp
+++ CrossFile/src/CrossFile.cpp
@@ -4,4 +4,28 @@
  *  Created on: Jul 29, 2011
  *      Author: user
  */
+#ifdef LINUX
 #include "CrossFile.h"
+CrossFile::CrossFile(char *fileName)
+{
+	this->_fileStreamId=open(fileName,O_RDWR|O_SYNC);
+}
+CrossFile::~CrossFile()
+{
+	close(this->_fileStreamId);
+}
+void CrossFile::Lseek(int offset)
+{
+	lseek(this->_fileStreamId,offset,SEEK_SET);
+}
+void* CrossFile::Read(int size)
+{
+	void* temp;
+	read(this->_fileStreamId,temp,size);
+	return temp;
+}
+void CrossFile::Write(void* value,int size)
+{
+	write(this->_fileStreamId,value,size);
+}
+#endif
--- CrossFile/src/CrossFile.h
+++ CrossFile/src/CrossFile.h
@@ -4,11 +4,26 @@
  *  Created on: Jul 29, 2011
  *      Author: user
  */
-
+#define LINUX
 #ifndef CROSSFILE_H_
 #define CROSSFILE_H_
+#ifdef LINUX
 
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-
+class CrossFile
+{
+private:
+	int _fileStreamId;
+public:
+	CrossFile(char *fileName);
+	~CrossFile();
+	void Lseek(int pos);
+	void* Read(int size);
+	void Write(void* value,int size);
+};
+#endif
 
 #endif /* CROSSFILE_H_ */
