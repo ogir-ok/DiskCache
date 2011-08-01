9fbf702cb4ebaf3645a2fa7ce399ef52b047400d
Garagatyy Igor <ogirok.07@mail.ru>
2011-08-01 14:35:19 +0300

    DiskBuff.h

--- DiskCache/src/DiskBuff.h
+++ DiskCache/src/DiskBuff.h
@@ -8,7 +8,16 @@
 #ifndef DISKBUFF_H_
 #define DISKBUFF_H_
 
-
+class DiskBuff
+{
+	int fsId;
+	int fsBlockNum;
+	void* pData;
+	DiskBuff* FreeNext;
+	DiskBuff* FreePrev;
+	DiskBuff* FullNext;
+	DiskBuff* FullPrev;
+};
 
 
 #endif /* DISKBUFF_H_ */
