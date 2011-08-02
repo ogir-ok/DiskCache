69bb7e16a25b196d4055f6fc3d07f9c9a5b57802
Garagatyy Igor <ogirok.07@mail.ru>
2011-08-02 14:07:28 +0300

    Hash

--- DiskCache/src/DiskBuffHashTable.h
+++ DiskCache/src/DiskBuffHashTable.h
@@ -4,11 +4,24 @@
  *  Created on: Aug 1, 2011
  *      Author: user
  */
+#define HASH_COUNT 10
 
 #ifndef DISKBUFFHASHTABLE_H_
 #define DISKBUFFHASHTABLE_H_
-
-
+#include "DiskBuffFullList.h"
+#include <vector>
+class DiskBuffHashTable
+{
+private:
+	vector<DiskBuffFullList>  _hash;
+	CrossPthreadRWLock _lock;
+public:
+	DiskBuffHashTable();
+	~DiskBuffHashTable();
+	void Add(DiskBuff* buff);
+	void Del(DiskBuff* buff);
+	DiskBuff* Get(int fsId,int blockNum);
+};
 
 
 #endif /* DISKBUFFHASHTABLE_H_ */
