f34062d0fe54ada775653d221c530b1610d41a53
Garagatyy Igor <ogirok.07@mail.ru>
2011-08-02 15:56:35 +0300

    v 0.2.0

---
+++ TestDiskCache/src/TestDiskCache.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TestDiskCache.cpp
+// Author      : Garagatyy Igor
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
