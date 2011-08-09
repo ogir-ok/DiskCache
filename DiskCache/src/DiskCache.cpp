effce5c4cc2d6666a80746b30274f81ce4e08d4a
Garagatyy Igor <ogirok.07@mail.ru>
2011-08-09 16:03:03 +0300

    edrtfyg

--- DiskCache/src/DiskCache.cpp
+++ DiskCache/src/DiskCache.cpp
@@ -53,6 +53,7 @@ BlockData DiskCache::Read(int fsId, int pos, int len)
 		}
 		ans->fsId = fsId;
 		ans->fsBlockNum = block;
+		ans->pData = new char[BLOCK_SIZE];
 		FSDriver::Instance()->GetBlock(fsId, block,&(ans->pData));
 		_diskBuffHashTable->Add(ans);
 	}
